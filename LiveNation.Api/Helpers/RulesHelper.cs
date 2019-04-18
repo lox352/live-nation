35dd01b68da90b3d1eff862d8f73cb18161726df
Lachlan Stibbard Hawkes <lachlan.s-hawkes@xero.com>
2019-04-18 11:09:15 +1000

    feat: Flesh out the ConvertRange method and create RulesHelper class

---
+++ LiveNation.Api/Helpers/RulesHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LiveNation.Api.Helpers
+{
+    public interface IRulesHelper
+    {
+        string ApplyRules(int integer, Dictionary<string, int> resultsSummary);
+    }
+
+    public class RulesHelper : IRulesHelper
+    {
+        public string ApplyRules(int integer, Dictionary<string, int> resultsSummary)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ LiveNation.Api/Services/ConvertRangeService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using LiveNation.Api.DTOs.Request;
+using LiveNation.Api.DTOs.Response;
+using LiveNation.Api.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveNation.Api.Services
+{
+    public interface IConvertRangeService
+    {
+        ConvertedRange ConvertRange(RangeRequest range);
+    }
+
+    public class ConvertRangeService : IConvertRangeService
+    {
+        private readonly IRulesHelper _rulesHelper;
+
+        public ConvertRangeService(IRulesHelper rulesHelper)
+        {
+            _rulesHelper = rulesHelper;
+        }
+
+        public ConvertedRange ConvertRange(RangeRequest range)
+        {
+            var countDictionary = new Dictionary<string, int>();
+
+            var integers = Enumerable.Range(range.Start, range.Length);
+            var convertedRange = integers.Select(integer => _rulesHelper.ApplyRule(integer, countDictionary));
+
+            return new ConvertedRange()
+            {
+                Result = string.Join(' ', convertedRange),
+                ResultSummary = countDictionary
+            };
+        }
+    }
+}
